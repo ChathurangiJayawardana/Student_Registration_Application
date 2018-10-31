245f60382b3f4aac3c92a56060a5df51d6dd6d13
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-01 02:44:39 +0530

    Controller classes for application first activity

---
+++ CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using StudentRegistrationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationApplication.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ViewResult Index()
+        {
+            var courses = GetCourses();
+            return View(courses);
+        }
+        private IEnumerable<Course> GetCourses()
+        {
+            return new List<Course>
+            {
+                new Course{ Id="CM1100",Name="Mathematics and Statistics"},
+                 new Course{ Id="IN1400",Name="Database Management System"},
+                 new Course{Id="Is1200",Name="Principals of Management"}
+            };
+        }
+    }
+}
---
+++ StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using StudentRegistrationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentReg.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ViewResult Index()
+        {
+            var students = GetStudents();
+            return View(students);
+        }
+        public ActionResult Details(string id)
+        {
+            var student = GetStudents().SingleOrDefault(c => c.Id == id);
+            if (student == null)
+                return HttpNotFound();
+            return View(student);
+        }
+        private IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student {Id="164001A", Name="Abeyrathne K.L.M."},
+                  new Student {Id="164002B", Name="Bandara M.Y."},
+                  new Student{Id="164003C",Name="De silva A.R."},
+                    new Student{Id="164004D",Name="Ekanayake T.D."}
+            };
+        }
+    }
+}
