8b857118055b94424eeb4554c33d9b9c945409ef
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-01 01:41:02 +0530

    Student Model class

---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationApplication.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string IndexNo { get; set; }
+    }
+}
