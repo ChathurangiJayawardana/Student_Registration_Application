8494640a145f1c3a00dc3caff7999a182478ed65
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-02 22:35:12 +0530

    View Models for first activity

---
+++ RandomCourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StudentRegistrationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationApplication.ViewModels
+{
+    public class RandomCourseViewModel
+    {
+        public Course Course { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
