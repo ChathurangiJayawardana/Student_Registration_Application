a80ce149684f14650b8109b9b22af20bbb45df03
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-04 19:03:00 +0530

    Add Properties to AcademicType Model

---
+++ AcademicType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationApplication.Models
+{
+    public class AcademicType
+    {
+        public byte Id { get; set; }
+        public int NoOfCourses { get; set; }
+        public int TotalCredits { get; set; }
+        public byte year { get; set; }
+
+    }
+}
