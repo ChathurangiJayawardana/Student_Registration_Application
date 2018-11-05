b0a7b0e903f1d6c4012b90ba29d6f39232108c26
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-05 22:05:24 +0530

    Add Display Names to properties of Student Model

--- Student.cs
+++ Student.cs
@@ -12,7 +12,9 @@ namespace StudentRegistrationApplication.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name="Index Number")]
         public string IndexNo { get; set; }
+        [Display(Name="Register Date")]
         public DateTime? RegisterDate { get; set; }
         public AcademicType AcademicType { get; set; }
         public byte AcademicTypeId { get; set; }
